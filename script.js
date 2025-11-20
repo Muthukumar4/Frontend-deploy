c1736b22d20500a25589d4153e86c98a0b5401cf
Muthukumar4 <kumararumugam825@gmail.com>
2025-11-20 10:07:44 +0530

    frontend deploy

---
+++ script.js
@@ -0,0 +1,41 @@
+function ShowCourses() {
+    fetch("https://backend-deploy-9th1.onrender.com/course")
+    .then((response) => response.json())
+    .then((courses) => {
+        const datatable = document.getElementById("coursetable");
+        datatable.innerHTML = ""; // optional: clear existing rows
+
+        courses.forEach(course => {
+            const row = `
+                <tr>
+                    <td>${course.coursId}</td>
+                    <td>${course.courseName}</td>
+                    <td>${course.trainer}</td>
+                    <td>${course.duration}</td>
+                </tr>
+            `;
+            datatable.innerHTML += row;
+        });
+    });
+}
+
+function EndrolledStundets() {
+    fetch("https://backend-deploy-9th1.onrender.com/enrolled")
+    .then((response) => response.json())
+    .then((students) => {
+        const datatable = document.getElementById("stundenttable");
+        datatable.innerHTML = ""; // optional: clear existing rows
+
+        students.forEach(student => {
+            const row = `
+                <tr>
+                    <td>${student.name}</td>
+                    <td>${student.emailId}</td>
+                    <td>${student.corsName}</td>
+                    
+                </tr>
+            `;
+            datatable.innerHTML += row;
+        });
+    });
+}
